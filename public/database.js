c9ff8683aec229c08ddb1457cab95fda4f87d500
Francesco Gaudenzi <fgaudo@gmail.com>
2016-01-10 12:59:01 +0100

    develop

--- public/database.js
+++ public/database.js
@@ -1,5 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(__ROOT + 'database.sqlite', sqlite3.OPEN_READONLY,
+var db = new sqlite3.Database(__ROOT + 'database/database.sqlite', sqlite3.OPEN_READONLY,
     function (err)
     {
         if (err)
