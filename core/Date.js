1874a3e65cef9a5bd6e53a3b4ceb95b1c898daf2
Francesco Gaudenzi <gaudo@gaudo-NB>
2016-04-11 18:33:03 +0200

    fix date

--- core/Date.js
+++ core/Date.js
@@ -1,7 +1,8 @@
 Date.prototype.toLocalISOString =
     function ()
     {
-        var date = [this.getFullYear(), ('0'+ (this.getMonth()+1)).slice(-2), ('0'+ this.getDate()).slice(-2)]
+        var month = this.getMonth()+1
+        var date = [this.getFullYear(), ('0'+ month).slice(-2), ('0'+ this.getDate()).slice(-2)]
         date = date.join('-')
 
         var time = [this.getHours(), this.getMinutes()]
